3bcfe6794fb369277b264c557175d7dffd5174d8
Mahmoud E <m.e.bestawy@gmail.com>
2020-05-07 07:06:55 -0400

    Complete and documented website

---
+++ Scripts/IncludeElements.js
@@ -0,0 +1,7 @@
+/* Script to dynamically add header and footer */
+$(document).ready(function() {
+    $("header").load("/HTML/HeaderMenu.html"); 
+    $("footer").load("/HTML/Footer.html"); 
+});
+
+
