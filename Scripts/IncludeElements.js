dcdcd4b8a562292dff0445441e084c0785593872
Mahmoud E <m.e.bestawy@gmail.com>
2020-05-07 06:58:25 -0400

    Revert "Initial commit"
    This reverts commit 66f3a0b8787b94f692b0f10760c10d76d19cc40d.

--- Scripts/IncludeElements.js
+++
@@ -1,7 +0,0 @@
-/* Script to dynamically add header and footer */
-$(document).ready(function() {
-    $("header").load("/HTML/HeaderMenu.html"); 
-    $("footer").load("/HTML/Footer.html"); 
-});
-
-
--- Scripts/SubmissionPageScript.js
+++
@@ -1,6 +0,0 @@
-/* Script for the form submission page */
-
-// click on exit button
-$(document).on("click", "#ExitButton", function(e) {
-    window.close();
-});
